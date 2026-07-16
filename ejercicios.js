b54c47ba0f3969e5d27a9d64e082520d2b01337b
Puntiana <adriana.punti@gmail.com>
2026-07-15 21:19:05 -0600

    Add ejercicios.js with multiple JavaScript exercises

---
+++ ejercicios.js
@@ -0,0 +1,50 @@
+//parte 1
+
+//Ejercicio 1
+
+console.log ("Hola Mundo");
+console.log ("Bienvenido a JavaScript");
+console.log ("Estoy aprendiendo programación");
+
+//Ejercicio 2
+
+console.log ("Adriana Punti");
+console.log ("37 años");
+console.log ("Ciudad de Guatemala");
+console.log ("Desarrolo de software");
+
+//Ejercicio 3
+
+console.log ("*");
+console.log ("**");
+console.log ("***");
+console.log ("****");
+console.log ("*****");
+console.log ("*******");
+console.log ("********");
+
+
+//parte 1
+
+//Ejercicio 4
+
+var x = 20;
+var y = 32;
+
+console.log (x)
+console.log (y)
+
+//Ejercicio 5
+
+console.log (x+y)
+console.log (y-x)
+console.log (y*x)
+console.log (x/y)
+console.log (x%y)
+console.log (x**y)
+
+//Ejercicio 6
+
+
+
+
